898f2f57e2e760a0f20b4e02b0cd4f86090a8aee
assafsun <assaf.sun@gmil.com>
2019-02-09 20:22:23 +0200

    Support in basic tabs name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
-  title = 'master-header';
+export class AppComponent implements OnInit {
+  public title = 'master-header';
+  public tabs: string[] = [];
+
+  public ngOnInit() {
+    this.tabs.push ("Assaf");
+    this.tabs.push ("Moran");
+    this.tabs.push ("Alon");
+    this.tabs.push ("Neta");
+  }
+
+  public onTabSelected(tab: string) {
+    console.log("Selected tab is " + tab);
+  }
 }
--- src/masterHeader/master-header.component.ts
+++ src/masterHeader/master-header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'master-header',
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MasterHeaderComponent implements OnInit {
 
+  @Input()
+  public tabs: string[] = [];
+
+  @Output()
+  tabSelected: EventEmitter<string> = new EventEmitter();
+
   constructor() { }
 
-  ngOnInit() {
+  public ngOnInit() {
   }
 
+  public selectTab(tabName: string) {
+    this.tabSelected.emit(tabName);
+  }
 }
